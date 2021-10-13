a3c48fffaf9ba0dcf7e41544dbb557c3f67b2baf
substack <substack@bits.coop>
2021-10-13 12:01:41 -1000

    use a request queue for fetches

--- storage/http.js
+++ storage/http.js
@@ -1,7 +1,11 @@
 var rx = 0
+var connectionLimit = 6
 
 module.exports = function (root) {
   var controllers = {}
+  var active = {}
+  var queue = []
+  var pending = 0
   var storageFn = function (name) {
     var data = null
     return {
@@ -18,72 +22,94 @@ module.exports = function (root) {
         cb(new Error('sync not implemented'))
       },
       length: function f (cb) {
-        if (data === null) getData(f, cb)
-        else cb(null, data.length)
+        if (data === null) {
+          getData(name, function (err, d) {
+            data = d
+            cb(null, data.length)
+          })
+        } else cb(null, data.length)
       },
       read: function f (offset, length, cb) {
-        //console.log(name,'read',offset,length)
-        if (data === null) getData(f, offset, length, cb)
-        else if (offset === 0 && length === data.length) {
+        if (data === null) {
+          getData(name, function (err, d) {
+            data = d
+            if (offset === 0 && length === data.length) {
+              cb(null, data)
+            } else {
+              cb(null, data.subarray(offset,offset+length))
+            }
+          })
+        } else if (offset === 0 && length === data.length) {
           cb(null, data)
         } else {
           cb(null, data.subarray(offset,offset+length))
         }
       },
     }
-    async function getData (f, ...args) {
-      console.log('get',name)
-      if (controllers[name] === null) { // cancelled request
-        storageFn.activeRequests.delete(name)
-        delete controllers[name]
-        return
-      } else if (controllers[name]) {
-        return // already open
+  }
+  async function getData(name, cb) {
+    if (active[name] || pending >= connectionLimit) {
+      queue.push({ name, cb })
+      return
+    }
+    pending++
+    console.log('get',name,pending,queue.length)
+    active[name] = true
+    var opts = {}
+    if (typeof AbortController !== 'undefined') {
+      controllers[name] = new AbortController
+      opts.signal = controllers[name].signal
+    }
+    var to = setTimeout(function () {
+      if (controllers[name]) controllers[name].abort()
+      delete controllers[name]
+    }, 5_000)
+    try {
+      data = Buffer.from(await (await fetch(root + '/' + name, opts)).arrayBuffer())
+      rx += data.length
+    } catch (err) {
+      if (controllers[name] !== null) {
+        queue.push({ name, cb })
+        setTimeout(next, 1_000)
+      } else {
+        console.error(err)
       }
-      var opts = {}
-      if (typeof AbortController !== 'undefined') {
-        controllers[name] = new AbortController
-        opts.signal = controllers[name].signal
-      }
-      var to = setTimeout(function () {
-        if (controllers[name]) controllers[name].abort()
-        delete controllers[name]
-      }, 5_000)
-      //console.log('open',name)
-      storageFn.activeRequests.add(name)
-      try {
-        data = Buffer.from(await (await fetch(root + '/' + name, opts)).arrayBuffer())
-        rx += data.length
-      } catch (err) {
-        console.log('CAUGHT',err,controllers[name] === null)
-        if (controllers[name] === null) {
-          storageFn.activeRequests.delete(name)
-        } else {
-          setTimeout(function () {
-            if (controllers[name] !== null) {
-              console.log('retry', name)
-              getData(f, ...args) // retry
-            }
-          }, 5_000)
+    }
+    clearTimeout(to)
+    delete active[name]
+    delete controllers[name]
+    if (data) {
+      try { cb(null, data) }
+      catch (err) { console.error(err) }
+      var found = false
+      for (var i = 0; i < queue.length; i++) {
+        if (queue[i].name === name) {
+          try { queue[i].cb(null, data) }
+          catch (err) { console.error(err) }
+          found = true
         }
       }
-      console.log('complete',name)
-      clearTimeout(to)
-      delete controllers[name]
-      if (data) {
-        storageFn.activeRequests.delete(name)
-        f.apply(null, args)
-        console.log((rx/1024/1024).toFixed(1) + ' M')
-      }
+      if (found) queue = queue.filter(q => q.name !== name)
+      //console.log((rx/1024/1024).toFixed(1) + ' M')
+    }
+    pending--
+    if (queue.length > 0 && pending < connectionLimit) {
+      setTimeout(next, 10)
     }
   }
   storageFn.cancel = function (name) {
-    storageFn.activeRequests.delete(name)
+    queue = queue.filter(q => q.name !== name)
     if (controllers[name]) {
       controllers[name].abort()
     }
     controllers[name] = null
   }
-  storageFn.activeRequests = new Set
   return storageFn
+
+  function next() {
+    if (queue.length > 0 && pending < connectionLimit) {
+      var q = queue.shift()
+      getData(q.name, q.cb)
+    }
+  }
 }
